d61235ae93b050ddc43b000ea6a1d5a63cf11022
Harshal Bhatia <harshalbhatia244@gmail.com>
2019-05-08 12:16:56 +0530

    Accept combined `urlEndpoint` & apply transformations through query params

--- src/components/Image.js
+++ src/components/Image.js
@@ -15,17 +15,13 @@ class Image extends React.Component {
           .join(",");
     });
     console.log("tr", transformationString);
-    return transformationString ? `tr${transformationString}/` : "";
+    return transformationString ? "?tr=" + transformationString : "";
   }
 
   linkBuilder() {
-    const urlEndpoint = `${this.props.httpMethod}://${this.props.subDomain}.${
-      this.props.domain
-    }/${this.props.imagekitId}`;
-    const transformationStringWithSlash = this.transformationBuilder();
-    return `${urlEndpoint}/${transformationStringWithSlash}${
-      this.props.imgPathWithExt
-    }`;
+    const urlEndpoint = this.props.urlEndpoint;
+    const transformationString = this.transformationBuilder();
+    return `${urlEndpoint}${this.props.path}${transformationString}`;
   }
 
   render() {
@@ -37,16 +33,13 @@ class Image extends React.Component {
 export default Image;
 
 Image.propTypes = {
-  imgPathWithExt: PropTypes.string.isRequired,
+  urlEndpoint: PropTypes.string.isRequired,
   alt: PropTypes.string,
   transformations: PropTypes.arrayOf(PropTypes.object),
-  imagekitId: PropTypes.string,
-  domain: PropTypes.string,
   httpMethod: PropTypes.oneOf("http", "https")
 };
 
 Image.defaultProps = {
-  domain: "imagekit.io",
   httpMethod: "https",
   transformations: []
 };
